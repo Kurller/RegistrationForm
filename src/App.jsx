2782196f00d896d30cfa9ca39e587477e58b636a
Kurller <kolaquadry@gmail.com>
2025-11-16 10:39:27 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { SuperheroForm } from "./form";
+
+function App() {
+  return (
+    <div>
+      <h1>Superhero Application Form</h1>
+      < SuperheroForm/>
+    </div>
+  );
+}
+
+export default App;
